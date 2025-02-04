0084f3163ec9a99d2ff08542bce1c89d9770f7e3
Durai1309 <durairaj14092000@gmail.com>
2025-02-04 23:29:58 +0530

    Created the BaseService

--- Ecommerce.Web/Service/BaseService.cs
+++ Ecommerce.Web/Service/BaseService.cs
@@ -1,13 +1,77 @@
 ﻿using Ecommerce.Web.Models;
 using Ecommerce.Web.Service.IService;
+using static Ecommerce.Web.Utility.SD;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace Ecommerce.Web.Service
 {
     public class BaseService : IBaseService
     {
-        public Task<Response?> SendAsync(Request request)
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BaseService(IHttpClientFactory httpClientFactory)
         {
-            throw new NotImplementedException();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<Response?> SendAsync(Request request)
+        {
+            try
+            {
+                HttpClient client = _httpClientFactory.CreateClient("API");
+                HttpRequestMessage message = new();
+                //token
+                message.RequestUri = new Uri(request.Url);
+                if (request.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(request.Data), Encoding.UTF8, "application/json");
+                }
+                HttpResponseMessage? apiResponse = null;
+
+                switch (request.ApiType)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+                switch (apiResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        return new() { IsSuccess = false, Message = "Not Found" };
+                    case HttpStatusCode.Forbidden:
+                        return new() { IsSuccess = false, Message = "Access Denied" };
+                    case HttpStatusCode.Unauthorized:
+                        return new() { IsSuccess = false, Message = "Unauthorized" };
+                    case HttpStatusCode.InternalServerError:
+                        return new() { IsSuccess = false, Message = "Internal Server Error" };
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var apiResponseDto = JsonConvert.DeserializeObject<Response>(apiContent);
+                        return apiResponseDto;
+                }
+            }
+            catch (Exception ex)
+            {
+                var dto = new Response
+                {
+                    Message = ex.Message.ToString(),
+                    IsSuccess = false
+                };
+                return dto;
+            }
         }
     }
 }
---
+++ Ecommerce.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Web.Models;
+
+namespace Ecommerce.Web.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<Response?> GetCouponAsync(string couponCode);
+        Task<Response?> GetAllCouponsAsync();
+        Task<Response?> GetCouponByIdAsync(int id);
+        Task<Response?> CreateCouponsAsync(Coupon couponDto);
+        Task<Response?> UpdateCouponsAsync(Coupon couponDto);
+        Task<Response?> DeleteCouponsAsync(int id);
+    }
+}

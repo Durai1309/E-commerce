b743ea7f0d1608c6a8f11fecd68a6a749a0cd683
Durai1309 <durairaj14092000@gmail.com>
2025-02-09 23:55:06 +0530

    Created the Auth Api

---
+++ Ecommerce.Services.AuthAPI/Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Services.AuthAPI.Models
+{
+    public class Response
+    {
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}

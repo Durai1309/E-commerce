9f7373c4319473fdb3694efb635ef39361b1799e
Durai1309 <durairaj14092000@gmail.com>
2025-02-14 23:44:48 +0530

    Worked on Login View

---
+++ Ecommerce.Web/Service/AuthService.cs
@@ -0,0 +1,56 @@
+﻿using Ecommerce.Web.Models;
+using Ecommerce.Web.Service.IService;
+using Ecommerce.Web.Utility;
+
+namespace Ecommerce.Web.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IBaseService _baseService;
+        public AuthService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<Response?> AssignRoleAsync(RegistrationRequest registrationRequest)
+        {
+            return await _baseService.SendAsync(new Request()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = registrationRequest,
+                Url = SD.AuthAPIBase + "/api/auth/AssignRole"
+            });
+        }
+
+        public Task<Response?> LoginAsync(LoginRequest loginRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response?> RegisterAsync(RegistrationRequest registrationRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*public async Task<Response?> LoginAsync(LoginRequest loginRequest)
+        {
+            return await _baseService.SendAsync(new Request()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = loginRequest,
+                Url = SD.AuthAPIBase + "/api/auth/login"
+            }, withBearer: false);
+
+        }
+
+        public async Task<Response?> RegisterAsync(RegistrationRequest registrationRequest)
+        {
+            return await _baseService.SendAsync(new Request()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = registrationRequest,
+                Url = SD.AuthAPIBase + "/api/auth/register"
+            }, withBearer: false);
+        }*/
+    }
+}
---
+++ Ecommerce.Web/Service/IService/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Ecommerce.Web.Models;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Web.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<Response?> LoginAsync(LoginRequest loginRequest);
+        Task<Response?> RegisterAsync(RegistrationRequest registrationRequest);
+        Task<Response?> AssignRoleAsync(RegistrationRequest registrationRequest);
+    }
+}
